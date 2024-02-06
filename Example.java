afca6772d1bb3ac1cd39a3da1ab24bc1ea852ae9
adityapandey03012003@gmail.com <104114546+03Adi@users.noreply.github.com>
2024-02-06 10:34:43 +0530

    About core java

---
+++ Example.java
@@ -0,0 +1,10 @@
+// public class Example {
+//     public static void main(String[] args) {
+//         int a, b;
+//         b=5;
+//         a=4;
+//         b -= a;
+
+//         System.out.println(b);
+//     }
+// }
---
+++ operators.java
@@ -0,0 +1,15 @@
+public class operators {
+    public static void main(String[] args) {
+        System.out.println("Operators in java");
+        // perform particular operation on provide operands
+        // 1. Arithemetic operator
+        // 2. conditional op
+        // 3. Assingnment op
+        // 4. composit op
+        // 5. relational op
+        // 6. logical op
+        // 7. bitwise op
+        // 8. increment/decrement op
+        
+    }
+}
