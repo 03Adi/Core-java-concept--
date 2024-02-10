d1e2efc8a8f9611ea7c6677a3dd87c39d3fc6559
adityapandey03012003@gmail.com <104114546+03Adi@users.noreply.github.com>
2024-02-10 22:05:20 +0530

    #Day04

--- ASCIIvalue.java
+++ ASCIIvalue.java
@@ -1,6 +1,30 @@
 // use of ASCII value to print the A B C D value in patttern printing qus
+// In this problem we are gernerally changes apply on the column---
+
+import java.util.*;
 public class ASCIIvalue {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        // System.out.println("Enter no : ");
+        // int n=sc.nextInt();
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         System.out.print((char)(j+64)+" ");// char are basically used for to chnage the int value to char value----
+
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // In this problem we are changes create in the 
+
+        // int n=sc.nextInt();
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         System.out.print((char)(i+64)+" ");
+        //     }
+        //     System.out.println();
+        // }
+
     }
 }
--- Example.java
+++ Example.java
@@ -1,10 +1,14 @@
-public class Example {
-    public static void main(String[] args) {
-        int a, b;
-        b=5;
-        a=4;
-        b -= a;
+// public class Example {
+//     public static void main(String[] args) {
+//         int a, b;
+//         b=5;
+//         a=4;
+//         b -= a;
 
-        System.out.println(b);
-    }
-}
+//         System.out.println(b);
+//     }
+// }
+
+
+// --- pattern related practice qus-----
+
